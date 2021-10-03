9acc362366803b8cabe11bbb63de1ff058ee9a1a
hideukin <hideukin@gmail.com>
2021-10-03 22:25:26 +0900

    React の導入

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,12 @@
+import React from 'react'
+import ReactDom from 'react-dom'
 import $ from 'jquery'
 import { Book, Review } from './app.d'
 import createBookReview from './createBookReview'
 import appendBook from './appendBook'
+
+const root = document.getElementById('react-root');
+ReactDom.render(<h1>React で描画する</h1>, root)
 
 $(function () {
   $.ajax('http://localhost:1323/books')
