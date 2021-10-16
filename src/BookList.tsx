3ed51c08be44c0a048378b9f9d22ea067a41b44c
hideukin <hideukin@gmail.com>
2021-10-16 23:42:45 +0900

    BookList と ReviewList を追加

---
+++ src/BookList.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import ReviewList from './ReviewList'
+import type { Book } from './app.d'
+
+export default function BookList({ books }: { books: Book[] }) {
+  const [showReview, setShowReview] = useState(false);
+  return (
+    <ul className="book-list">
+      {books.map(book => (
+        <li className="book-list__item">
+          <div className="book-list__item__inner">
+            <img className="book-list__item__inner__image" src={book.image} alt={book.title} />
+            <div className="book-list__item__inner__info">
+              <h3 className="book-list__item__inner__info__title">
+                {book.title}
+                <span className="book-list__item__inner__info__title__author">
+                  ({book.author})
+                </span>
+              </h3>
+              <p className="book-list__item__inner__info__overview">{book.overview}</p>
+              <p className="book-list__item__inner__info__comment">
+                <a className="book-list__item__inner__info__comment__link" onClick={() => { setShowReview(!showReview) }}>
+                  {book.reviews.length}件の感想・評価
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="review">
+            <ReviewList reviews={book.reviews} />
+            <form className="review__form">
+              <textarea className="review__form__input" rows={5} placeholder={`「${book.title}」を読んだ感想・評価を教えてください`}></textarea>
+              <button className="review__form__submit" type="submit">投稿</button>
+            </form>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
